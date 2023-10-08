dd19c684f5119bcbfc7f827e0536aaa3e8f499cb
andrew <andrewongek@gmail.com>
2023-10-08 21:08:23 +0800

    add cache set func

--- cache/itemInfo.go
+++ cache/itemInfo.go
@@ -29,8 +29,8 @@ func NewOrderInfo(address string) *ItemInfo {
 	}
 }
 
-func (i *ItemInfo) GetItemInfo(itemid int64) (structs.ItemData, error) {
-	res := structs.ItemData{}
+func (i *ItemInfo) GetItemInfo(itemid int64) (*structs.ItemData, error) {
+	res := &structs.ItemData{}
 	ctx := context.Background()
 	val, err := i.redis.Get(ctx, fmt.Sprintf(keyTemplate, itemid)).Result()
 	if err != nil {
@@ -43,4 +43,22 @@ func (i *ItemInfo) GetItemInfo(itemid int64) (structs.ItemData, error) {
 	}
 
 	return res, nil
+}
+
+func (i *ItemInfo) SetItemInfo(itemData *structs.ItemData) error {
+	ctx := context.Background()
+	data, err := json.Marshal(itemData)
+	if err != nil {
+		return fmt.Errorf("marshal item info err=%s", err)
+	}
+	key := getItemInfoKey(itemData.Id)
+	err = i.redis.Set(ctx, key,  data, itemCacheTTL).Err()
+	if err != nil {
+		return fmt.Errorf("item info cache err=%s", err.Error())
+	}
+	return nil 
+}
+
+func getItemInfoKey(id int64) string {
+	return fmt.Sprintf(keyTemplate, id)
 }
